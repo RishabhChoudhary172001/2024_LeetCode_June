037d3222ecfb89867323cb16f90574a2910e506d
RishabhChoudhary172001 <rishabhchoudhary172001@gmail.com>
2024-06-04 00:22:52 +0530

    changes to the max consectuive ones

---
+++ 4_june/max_consecutive_ones_iii/max_consectuive_ones_iii.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int longestOnes(vector<int> &nums, int k)
+    {
+        int maxSize = 0;
+        int currCoun = 0;
+
+        int i = 0, j = 0;
+        while (j < nums.size())
+        {
+            if (nums[j] == 0)
+            {
+                currCoun++;
+            }
+            while (currCoun > k)
+            {
+                if (nums[i] == 0)
+                {
+                    currCoun--;
+                }
+
+                i++;
+            }
+            maxSize = max(maxSize, (j - i) + 1);
+            j++;
+        }
+        return maxSize;
+    }
+};
